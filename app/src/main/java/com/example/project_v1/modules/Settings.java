6615e3dbce4d2782d690f6db2aec2170a4ecf6ab
kaarthi14 <kaarthipan2013@gmail.com>
2020-11-20 22:53:56 -0500

    password reset

--- app/src/main/java/com/example/project_v1/modules/Settings.java
+++ app/src/main/java/com/example/project_v1/modules/Settings.java
@@ -1,23 +1,35 @@
 package com.example.project_v1.modules;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.project_v1.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Settings extends AppCompatActivity {
 
     private FirebaseDatabase database;                          //All database data
-    private DatabaseReference mDatabase;                        //user's info (name, email, password and devices)
+    private DatabaseReference mDatabase;//user's info (name, email, password and devices)
+    private FirebaseAuth mAuth;
+
     private static final String USER = "user";
     private  String userID;
+    private String UID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +39,17 @@ public class Settings extends AppCompatActivity {
         //Database related
         database = FirebaseDatabase.getInstance();
         mDatabase = database.getReference(USER);
+        mAuth = FirebaseAuth.getInstance();
+
 
         Intent intent = getIntent();
         userID = intent.getStringExtra(("userID"));
+        UID=userID;
 
+
+        final FirebaseUser userr = mAuth.getCurrentUser();
+
+        if(userr==null){Toast.makeText(getApplicationContext(),"USER NULL", Toast.LENGTH_LONG).show();  }
 
         Button resetPasswordButton;
         EditText emailEditText;
@@ -49,12 +68,51 @@ public class Settings extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+            final EditText resetPassword = new EditText(view.getContext());
 
+            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+            passwordResetDialog.setTitle("Reset Password?");
+            passwordResetDialog.setMessage("Enter Your Email To Receive Reset Link");
+            passwordResetDialog.setView(resetPassword);
 
+            passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 
-
-            }
-        });
+                public void onClick(DialogInterface dialogInterface, int i) {
+String newPassword= resetPassword.getText().toString();
+userr.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+    @Override
+    public void onSuccess(Void aVoid) {
+        Toast.makeText(Settings.this,"Password Reset Sucessfully",Toast.LENGTH_LONG).show();
 
     }
-}
+}).addOnFailureListener(new OnFailureListener() {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        Toast.makeText(Settings.this,"Password Reset Failed",Toast.LENGTH_LONG).show();
+
+    }
+});
+                }
+            });
+
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+passwordResetDialog.create().show();
+
+            }
+
+
+
+
+
+
+
+            });
+        }
+
+    }
